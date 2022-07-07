0fc1feb1d657a0950eae9d5899b8c5fb1ae06df3
Aoki-gif <63574609+Aoki-gif@users.noreply.github.com>
2022-07-07 22:01:55 +0900

    json3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { Children, useState, useEffect } from "react";
-import { ChildArea } from "./ChildArea";
-import Data from "./json/test.json";
 import Data2 from "./json/test2.json";
 import { useWindowSize } from "react-use";
 import "./styles.css";
@@ -8,17 +6,10 @@ import "./styles.css";
 export default function App() {
   //const [count, setCount] = useState(0);
   const { width, height } = useWindowSize();
-
   const childrenItems = Data2;
-  const mainStyle = {
-    height: childrenItems[0].height,
-    width: childrenItems[0].width,
-    border: "solid",
-    position: "relative"
-  };
-
   const metaData = childrenItems[0];
-  //console.log(childrenItems);
+  console.log(width);
+  console.log(height);
   // console.log(metaData);
 
   return (
@@ -29,38 +20,26 @@ export default function App() {
         ItemsStyleInf = {
           height: object.height,
           width: object.width,
-          border: "solid",
           position: "relative"
         };
       } else {
         ItemsStyleInf = {
           x: (object.x / metaData.width) * width,
           y: (object.y / metaData.height) * height,
-          height: object.height,
-          width: object.width,
-          border: "solid",
+          height: (object.height / metaData.height) * height,
+          width: (object.width / metaData.width) * width,
           position: "absolute",
           top: (object.y / metaData.height) * height,
           left: (object.x / metaData.width) * width
         };
       }
       //console.log(ItemsStyleInf);
-      console.log(object.text);
+      //console.log(object.text);
       return (
-        <div style={ItemsStyleInf}>
+        <div style={ItemsStyleInf} class={object.object_type}>
           {object.text ? <p>{object.text}</p> : undefined}
         </div>
       );
     })
-  );
-}
-
-//コンテナ作成
-function ItemContainer({ Style, Text }) {
-  console.log(Style);
-  return (
-    <div style={Style}>
-      <p>{Text}</p>
-    </div>
   );
 }
